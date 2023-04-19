7f6be894a6eaef9311c6e6579e0697cad81f9360
Kayw Lee <kwooler@gmail.com>
2023-04-18 20:18:15 -0700

    updates

--- main.hpp
+++ main.hpp
@@ -25,25 +25,7 @@ void printvector(vector<int> vec)
 }
 void insertone(vector<int> &vec, int usernum)
 {
-	vector<int>::iterator it;
-	for (it = vec.begin(); it != vec.end(); it++)
-	{
-		if (*it > usernum)
-			break;
-	}
-	vec.insert(it, usernum);
 }
 int deleteone(vector<int> &vec, int usernum)
 {
-	int cnt = 0;
-	vector<int>::iterator it;
-
-	it = find(vec.begin(), vec.end(), usernum);
-	while (it != vec.end())
-	{
-		vec.erase(it);
-		cnt += 1;
-		it = find(vec.begin(), vec.end(), usernum);
-	}
-	return cnt;
 }
